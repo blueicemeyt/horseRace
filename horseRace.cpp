d96d550b90539847abc99a887ae4619c1f171bef
blueicemeyt <blueicemeyt@gmail.com>
2026-09-25 15:40:23 +0000

    Update game

--- horseRace.cpp
+++ horseRace.cpp
@@ -24,7 +24,58 @@ void Horse::init(int i, int length){
   position = 0;
 }
 
-int coin = rand() % 2;
-if (coin == 0){
-  position++;
+void Horse::advance(){
+  int coin = rand() % 2;
+  if (coin == 1){
+    position++;
+  }
+}
+
+void Horse::printLane(){
+  for (int pos = 0; pos <= trackLength; pos++){
+    if (position == pos){
+      cout << ".";
+    }
+    else{
+      cout << ".";
+    }
+  }
+  cout << endl;
+}
+
+bool Horse::isWinner(){
+  bool win = false;
+  if (position >= trackLength{
+    win = true;
+    cout << "Horse " << index << " wins!" << endl;
+  }
+  return win;
+}
+
+
+
+// Race class
+
+class Race{
+public:
+  Race();
+  void start();
+
+private:
+  int NUM_HORSES;
+  int TRACK_LENGTH;
+  Horse horses[5]
+};
+
+Race::Race(){
+  const int TRACK_LENGTH = 15;
+  const int NUM_HORSES = 5;
+
+  Horse horses[NUM_HORSES];
+  for(int i = 0; i < NUM_HORSES; i++){
+    horses[i].init(i, TRACK_LENGTH);
+
+
+
+
 
